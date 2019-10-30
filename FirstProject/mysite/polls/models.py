d913dd29c0741d969c841adc9de5eb96e7ec235c
junaid <junaid.khurshid@mparsec.com>
2019-10-30 12:25:00 +0500

    (Created Models)

--- FirstProject/mysite/polls/models.py
+++ FirstProject/mysite/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Publish Date')
+
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    Num_of_votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
