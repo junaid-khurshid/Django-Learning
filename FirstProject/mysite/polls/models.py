15b67c3f930bf914a7565a7cdc61c58931fd96b1
juni381 <juni381.sj@gmail.com>
2019-11-01 13:26:09 +0500

    populating database by shell and admin site.

--- FirstProject/mysite/polls/models.py
+++ FirstProject/mysite/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -7,8 +8,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Publish Date')
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     Num_of_votes = models.IntegerField(default=0)
-    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
