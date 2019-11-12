e4ff6fe9be6d85042346b23068320b61801beca6
juni381 <junaid381.sj@gmail.com>
2019-11-12 20:33:21 +0500

    added pages and urls in polls

--- FirstProject/mysite/polls/urls.py
+++ FirstProject/mysite/polls/urls.py
@@ -1,9 +1,12 @@
-#from django.urls import path
-from django.conf.urls import url
+from django.urls import path
+# from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    #path('', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('', views.results, name='results'),
+    path('', views.vote, name='vote'),
+
 ]
